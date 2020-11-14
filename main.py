a162f6ecbd54054d7c32a403085479bc868fa827
Pra10sun <49338136+Pra10sun@users.noreply.github.com>
2020-11-14 14:30:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ smile_cascade = cv2.CascadeClassifier('frecog/haarcascade_smile.xml')
 
 
 
+
+
+
 def main():
 	"""Face Detection App"""
 
@@ -91,6 +94,59 @@ def main():
 			elif feature_choice == 'Cannize':
 				result_canny = cannize_image(our_image)
 				st.image(result_canny)
+def detect_faces(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the faces
+	for (x, y, w, h) in faces:
+				 cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img,faces 
+
+
+def detect_eyes(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+	for (ex,ey,ew,eh) in eyes:
+	        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	return img
+
+def detect_smiles(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect Smiles
+	smiles = smile_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the Smiles
+	for (x, y, w, h) in smiles:
+	    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img
+
+def cartonize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Edges
+	gray = cv2.medianBlur(gray, 5)
+	edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+	#Color
+	color = cv2.bilateralFilter(img, 9, 300, 300)
+	#Cartoon
+	cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+	return cartoon
+
+
+def cannize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	img = cv2.GaussianBlur(img, (11, 11), 0)
+	canny = cv2.Canny(img, 100, 150)
+	return canny				
 
 
 
