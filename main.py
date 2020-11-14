df2a14baf53ec4462bc07ad491f55705678df01b
Pra10sun <49338136+Pra10sun@users.noreply.github.com>
2020-11-14 15:08:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,73 @@
+# Core Pkgs
 import streamlit as st 
 import cv2
 from PIL import Image,ImageEnhance
 import numpy as np 
 import os
 
+@st.cache
+def load_image(img):
+	im = Image.open(img)
+	return im
+
 
 face_cascade = cv2.CascadeClassifier('frecog/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('frecog/haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('frecog/haarcascade_smile1.xml')
+smile_cascade = cv2.CascadeClassifier('frecog/haarcascade_smile.xml')
+
+def detect_faces(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the faces
+	for (x, y, w, h) in faces:
+				 cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img,faces 
 
 
+def detect_eyes(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+	for (ex,ey,ew,eh) in eyes:
+	        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	return img
+
+def detect_smiles(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Detect Smiles
+	smiles = smile_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the Smiles
+	for (x, y, w, h) in smiles:
+	    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	return img
+
+def cartonize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	# Edges
+	gray = cv2.medianBlur(gray, 5)
+	edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+	#Color
+	color = cv2.bilateralFilter(img, 9, 300, 300)
+	#Cartoon
+	cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+	return cartoon
+
+
+def cannize_image(our_image):
+	new_img = np.array(our_image.convert('RGB'))
+	img = cv2.cvtColor(new_img,1)
+	img = cv2.GaussianBlur(img, (11, 11), 0)
+	canny = cv2.Canny(img, 100, 150)
+	return canny
 
 def main():
 	"""Face Detection App"""
@@ -31,37 +89,36 @@ def main():
 			# st.write(type(our_image))
 			st.image(our_image)
 
-		enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale","Contrast","Brightness","Blurring"])
-		if enhance_type == 'Gray-Scale':
-			new_img = np.array(our_image.convert('RGB'))
-			img = cv2.cvtColor(new_img,1)
-			gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-			# st.write(new_img)
-			st.image(gray)
-		elif enhance_type == 'Contrast':
-			c_rate = st.sidebar.slider("Contrast",0.5,3.5)
-			enhancer = ImageEnhance.Contrast(our_image)
-			img_output = enhancer.enhance(c_rate)
-			st.image(img_output)
+			enhance_type = st.sidebar.radio("Enhance Type",["Original","Gray-Scale","Contrast","Brightness","Blurring"])
+			if enhance_type == 'Gray-Scale':
+				new_img = np.array(our_image.convert('RGB'))
+				img = cv2.cvtColor(new_img,1)
+				gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+				# st.write(new_img)
+				st.image(gray)
+			elif enhance_type == 'Contrast':
+				c_rate = st.sidebar.slider("Contrast",0.5,3.5)
+				enhancer = ImageEnhance.Contrast(our_image)
+				img_output = enhancer.enhance(c_rate)
+				st.image(img_output)
 
-		elif enhance_type == 'Brightness':
-			c_rate = st.sidebar.slider("Brightness",0.5,3.5)
-			enhancer = ImageEnhance.Brightness(our_image)
-			img_output = enhancer.enhance(c_rate)
-			st.image(img_output)
+			elif enhance_type == 'Brightness':
+				c_rate = st.sidebar.slider("Brightness",0.5,3.5)
+				enhancer = ImageEnhance.Brightness(our_image)
+				img_output = enhancer.enhance(c_rate)
+				st.image(img_output)
 
-		elif enhance_type == 'Blurring':
-			new_img = np.array(our_image.convert('RGB'))
-			blur_rate = st.sidebar.slider("Brightness",0.5,3.5)
-			img = cv2.cvtColor(new_img,1)
-			blur_img = cv2.GaussianBlur(img,(11,11),blur_rate)
-			st.image(blur_img)
-           
-		elif enhance_type == 'Original':
-			st.image(our_image,width=300)
-		else:
-			st.image(our_image,width=300)
-
+			elif enhance_type == 'Blurring':
+				new_img = np.array(our_image.convert('RGB'))
+				blur_rate = st.sidebar.slider("Brightness",0.5,3.5)
+				img = cv2.cvtColor(new_img,1)
+				blur_img = cv2.GaussianBlur(img,(11,11),blur_rate)
+				st.image(blur_img)
+			elif enhance_type == 'Original':
+				
+				st.image(our_image,width=300)
+			else:
+				st.image(our_image,width=300)
 
 
 		# Face Detection
@@ -90,16 +147,17 @@ def main():
 			elif feature_choice == 'Cannize':
 				result_canny = cannize_image(our_image)
 				st.image(result_canny)
-				
-				
 
 
-def cannize_image(our_image):
-	new_img = np.array(our_image.convert('RGB'))
-	img = cv2.cvtColor(new_img,1)
-	img = cv2.GaussianBlur(img, (11, 11), 0)
-	canny = cv2.Canny(img, 100, 150)
-	return canny
+
+
+	elif choice == 'About':
+		st.subheader("About Face Detection App")
+		st.markdown("Built with Streamlit by [Gourav Ojha](https://github.com/gouravojha)")
+		st.text("Gourav Ojha")
+		
+
+
 
 if __name__ == '__main__':
 		main()	
